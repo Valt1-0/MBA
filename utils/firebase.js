e0e08c94f45bcfdf2890288215954ce63b34d1ad
Valt1-0 <lai2val1@gmail.com>
2024-10-21 00:04:47 +0200

    Remove unused files and code, and update .gitignore

---
+++ utils/firebase.js
@@ -0,0 +1,27 @@
+import { initializeApp } from "firebase/app";
+// import {...} from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// Optionally import the services that you want to use
+// import {...} from "firebase/database";
+// import {...} from "firebase/functions";
+// import {...} from "firebase/storage";
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+// For more information on how to access Firebase in your project,
+// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
+
+export { app, db };
