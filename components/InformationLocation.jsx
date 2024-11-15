d561d9a072dfa60250eab54d9a23fe2834008ff8
Valt1-0 <lai2val1@gmail.com>
2024-11-15 01:53:15 +0100

    Suppression d'un log inutile dans le composant InformationLocation pour améliorer la propreté du code.

--- components/InformationLocation.jsx
+++ components/InformationLocation.jsx
@@ -28,7 +28,6 @@ export default function ContactInfo({ selectedPlace }) {
     }
   }, [selectedPlace]);
 
-  console.log("selectedPlace", selectedPlace.images);
 
   const handleDelete = async () => {
     try {
